4e969548e9bc8105fb7222f2f8c84ff9a68943cf
huanran <huanranjoe@163.com>
2018-09-30 00:30:47 +0800

    添加分类和标签信息接口

--- src/controller/Post/TermsTaxon.ts
+++ src/controller/Post/TermsTaxon.ts
@@ -8,14 +8,16 @@ export async function termFindById(context: Context) {
   const term = getManager().getRepository(Terms);
   const taxonomys = getManager().getRepository(Taxonomy);
 
-  const taxonmy = taxonomys.create(context.request.body);
+  const taxonmy = await taxonomys.findOne(context.request.body);
   const terms = await term.findOne(context.request.body);
 
   context.type = "application/json";
   context.body =  {
     message: {
-      term: terms,
-      taxonmy
+      id: terms.id,
+      term: terms.name,
+      nameTaxo: taxonmy.nameTaxon,
+      taxonmyDes: taxonmy.description
     }
   };
 }
